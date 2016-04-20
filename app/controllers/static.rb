d9bce526e313268c5009e85e994ca27d7ab612ac
nicolekoh <niicolekws@gmail.com>
2016-04-20 12:06:11 +0800

    bitlyn

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,5 +1,10 @@
+
+
+
+
 get '/' do
-	puts 'ACCESSING ROOT PATH -------------------------'
+
+ @urls = Url.all
   erb :"static/index"
 end
 
@@ -7,31 +12,43 @@ post '/urls' do
 	puts 'POSTING DATA TO URLS PATH -------------------------'
 	puts "PARAMS: #{params}"
 	long_url = params[:long_url] #used param to get the key=long_url(in the form), value=long url
-	my_url = Url.new(long_url: long_url) # create a new object to hold the long url
-    @short_url = my_url.shorten #calling the shorten method to shorten the long url to short
-    # my_url.update(short_url: @short_url) # create another object to hold the short url 
-    my_url.save
+	short_url = Url.shorten
+	my_url = Url.new(long_url: long_url, short_url: short_url) # create a new object to hold the long and short url
+     if my_url.save
+    redirect '/'
+else 
+	@notes = my_url.errors.full_messages
+
+	end
     # my_url.short_url # calling the short url. 
 
-    erb :"static/index", locals: {url:my_url.short_url } #call my_url.short_url is calling shorten method at url.rb
+    # erb :"static/index", 
+    # locals: {url:my_url.short_url } #call my_url.short_url is calling shorten method at url.rb
 end 
 
 get'/count' do 
 	@url = Url.all
-	erb :"static/count"
+	erb :"static/count" #link back url to user 
 end 
 
 get '/:short_url' do 
 	short_url = params[:short_url] #find the 6 digit short url 
 	final_result= Url.find_by(short_url: short_url) # find the short url match the long url in the database 
-	link = final_result.long_url # <Url id: 13, long_url: "http://www.w3schools.com/html/html_basic.asp", short_url: "EeUyI0">
+
+	# link = final_result.long_url # <Url id: 13, long_url: "http://www.w3schools.com/html/html_basic.asp", short_url: "EeUyI0">
 	count = final_result.counter.to_i 
-	count +=1 
+	count +=1
 	final_result.counter = count 
 	final_result.save 
-	redirect "http://" + final_result.long_url
+	redirect final_result.long_url
 
 	# find my long url , use short one find long one, redirect user to browser 
+end 
+
+get "/delete/:short_url" do 
+	@url = Url.find_by(short_url: params[:short_url])
+	@url.destroy
+	redirect '/'
 end 
 
 
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,10 +1,13 @@
 class Url < ActiveRecord::Base
 	#before_create :shorten #before you create a new entry run this first. 
 	# This is Sinatra! Remember to create a migration!
-	def shorten 
+
+	validates :long_url, presence: true
+	validates :long_url, format: { with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/, message: "sorry its not a URL" }
+
+	def self.shorten 
 		 range = [*'0'..'9',*'A'..'Z',*'a'..'z']
 		 range.sample 
 		 string = Array.new(6){range.sample}.join
-		 self.short_url = string
 	end 
 end
